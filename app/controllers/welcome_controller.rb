7feb38e0d9c9741309f8123273ee45ac988b300b
nazrul <nazrulku07@gmail.com>
2014-06-02 23:51:05 +0600

    add nav and dropdown settings menu

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,25 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user, :except => ['index']
+  layout :resolve_layout
 
-  def home
+  def index
 
   end
 
   def dashboard
    @teams = Team.all
+  end
+
+
+  private
+
+  def resolve_layout
+    case action_name
+      when "index"
+        "home"
+      else
+        "application"
+    end
   end
 
 end
