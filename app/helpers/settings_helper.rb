3a759de925832c856e4f6a47ed3df322b876366b
nazrul <nazrulku07@gmail.com>
2014-06-06 00:51:09 +0600

    fixed serrings issue

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,7 +1,14 @@
 module SettingsHelper
 
   def get_bgcolor
+    if !current_user.present?
+      return "EEE"
+    end
     @bgcolor = current_user.settings.find_by_key('bgcolor')
-    @bgcolor.value
+    if(@bgcolor.nil?)
+      "EEE"
+    else
+      @bgcolor.value
+    end
   end
 end
