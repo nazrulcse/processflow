f76301dee6cd4bcff0dd34d8c07353e3e2f327cd
Mahin <mahin.cse07@gmail.com>
2014-06-14 14:44:35 +0600

    local changes for task

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+
+  def remaining(task)
+    if(task.spend.nil?)
+      return task.effort
+    elsif(task.effort.nil?)
+      return task.spend
+    else
+      return task.effort - task.spend
+   end
+  end
+
 end
