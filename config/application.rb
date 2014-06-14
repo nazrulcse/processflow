5a9ed10d12fc1baa18b7840397de3fddffa7b7fc
nazrul <nazrulku07@gmail.com>
2014-06-14 21:16:33 +0600

    disabled assets pipelone

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ProcessFlow
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.enabled = false
+    config.assets.precompile = []
     config.assets.initialize_on_precompile = false
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
