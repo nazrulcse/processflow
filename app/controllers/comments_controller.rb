9d20a86a145d0018854cf9b414e614c03d6289f6
mahabubziko <mahabub_cse07@yahoo.com>
2014-06-14 14:02:38 +0600

    delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -52,8 +52,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
+        format.js {render :layout => false }
     end
   end
 
