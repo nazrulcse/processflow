f63d635faae1a3229a6de91af02c32537983ea2c
Mahin <mahin.cse07@gmail.com>
2014-06-15 19:20:11 +0600

    edit process time and priority and design assigned people list

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,12 +1,12 @@
 module TasksHelper
 
-  def remaining(task)
+  def remaining_effort(task)
     if(task.spend.nil?)
-      return task.effort
+      return task.effort.to_f
     elsif(task.effort.nil?)
-      return task.spend
+      return task.spend.to_f
     else
-      return task.effort - task.spend
+      return task.effort.to_f - task.spend.to_f
    end
   end
 
