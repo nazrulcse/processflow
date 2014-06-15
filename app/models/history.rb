59fea7bdd351dd31483c7c7a377b03923a050fdf
nazrul <nazrulku07@gmail.com>
2014-06-15 21:21:36 +0600

    implement notofication

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,7 @@
 class History < ActiveRecord::Base
   belongs_to :task
   belongs_to :user
+
+  scope :notification, -> (project_id) {joins("INNER JOIN tasks ON tasks.id = histories.task_id " +
+      "INNER JOIN projects ON projects.id = tasks.project_id where tasks.project_id = #{project_id}")}
 end
