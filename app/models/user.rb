7e3f21a7e22282f795ea529530506af9e57c6bca
unknown <ZIKO@.(none)>
2015-02-15 21:38:51 +0600

    Facebook Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  devise :omniauthable
   has_many :teams
   has_many :settings
   has_many :notification_subcriptions
@@ -15,4 +15,22 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 5, maximum: 120}, on: :create
   validates :password, length: {minimum: 5, maximum: 120}, on: :update, allow_blank: true
   validates :current_password, length: {minimum: 5, maximum: 120}, on: :update, allow_blank: true
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(:name => auth.extra.raw_info.name,
+                               :provider => auth.provider,
+                                       :uid => auth.uid,
+                                          :email => auth.info.email,
+                                               :password => Devise.friendly_token[0,20] )
+      end
+    end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,6 +20,8 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+  require "omniauth-facebook"
+  config.omniauth :facebook, "347881058733424", "b8c0cca6eaa447f814f6c5a05e763cf2" , {:client_options => {:ssl => {:verify => false}}}
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
