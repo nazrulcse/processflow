582817942493f3597a37837526cd823fa0dee68f
nazrul <nazrulku07@gmail.com>
2016-07-31 20:32:01 +0600

    Devise error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,13 +51,13 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) do |u|
+    devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :zip, :email, :image, :password, :password_confirmation)
     end
-    devise_parameter_sanitizer.for(:sign_up) do |u|
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :country, :zip, :username, :image, :email, :password, :password_confirmation)
     end
-    devise_parameter_sanitizer.for(:sign_in) do |u|
+    devise_parameter_sanitizer.permit(:sign_in) do |u|
       u.permit(:username, :email, :password)
     end
   end
